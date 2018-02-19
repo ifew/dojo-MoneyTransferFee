c63bbddf46a40fcc7dc9b4a08b95ce0bcbf84c21
iFew <chitpong@gmail.com>
2018-02-19 14:34:02 +0700

    Added Unit Test Same Bank Amount 0THB

---
+++ MoneyTransfer.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MoneyTransfer
+{
+    public class MoneyTransfer
+    {
+
+        internal bool Transfer(string originBank, string destinationBank, int v)
+        {
+            return false;
+        }
+    }
+}
--- MoneyTransferTest.cs
+++ MoneyTransferTest.cs
@@ -6,9 +6,15 @@ namespace MoneyTransfer
     public class MoneyTransferTest
     {
         [Fact]
-        public void Test1()
+        public void When_Transfer_Same_Bank_Amount_0THB_Should_Be_Return_False()
         {
+            var originBank = "SCB";
+            var destinationBank = "SCB";
 
+            MoneyTransfer moneyTransfer = new MoneyTransfer();
+            var actual = moneyTransfer.Transfer(originBank, destinationBank, 0);
+
+            Assert.False(actual);
         }
     }
 }
